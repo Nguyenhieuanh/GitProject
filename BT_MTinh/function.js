b5928effef07dd9bd1955f3840f51b2c8868ca67
nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-03-05 15:36:54 +0700

    thêm bài tập và sửa bài

--- BT_MTinh/function.js
+++ BT_MTinh/function.js
@@ -1,38 +1,39 @@
 function
-clickbutton(obj) {
+clickbutton(obj) { 
+    let output = document.getElementById('result');
     if (obj === '1') {
-        document.getElementById('result').value += 1;
+        output.value += 1;
     } else if (obj === '2') {
-        document.getElementById('result').value += 2;
+        output.value += 2;
     } else if (obj === '3') {
-        document.getElementById('result').value += 3;
+        output.value += 3;
     } else if (obj === '4') {
-        document.getElementById('result').value += 4;
+        output.value += 4;
     } else if (obj === '5') {
-        document.getElementById('result').value += 5;
+        output.value += 5;
     } else if (obj === '6') {
-        document.getElementById('result').value += 6;
+        output.value += 6;
     } else if (obj === '7') {
-        document.getElementById('result').value += 7;
+        output.value += 7;
     } else if (obj === '8') {
-        document.getElementById('result').value += 8;
+        output.value += 8;
     } else if (obj === '9') {
-        document.getElementById('result').value += 9;
+        output.value += 9;
     } else if (obj === '0') {
-        document.getElementById('result').value += 0;
+        output.value += 0;
     } else if (obj === '+') {
-        document.getElementById('result').value += '+';
+        output.value += '+';
     } else if (obj === '-') {
-        document.getElementById('result').value += '-';
+        output.value += '-';
     } else if (obj === '*') {
-        document.getElementById('result').value += '*';
+        output.value += '*';
     } else if (obj === '/') {
-        document.getElementById('result').value += '/';
+        output.value += '/';
     } else if (obj === 'C') {
-        document.getElementById('result').value = '';
+        output.value = '';
     } else if (obj === '<-') {
-        document.getElementById('result').value = document.getElementById('result').value.substr(0, document.getElementById('result').value.length - 1);
+        output.value = output.value.substr(0, output.value.length - 1);
     } else {
-        document.getElementById('result').value = eval((document.getElementById('result').value))
+        output.value = eval((output.value))
     }
 }
