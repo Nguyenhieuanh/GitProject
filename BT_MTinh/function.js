9f2359eb2c486f99b8f54467de9bd0a53a18616a
nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-03-05 16:59:14 +0700

    sửa lại bài tập máy tính

--- BT_MTinh/function.js
+++ BT_MTinh/function.js
@@ -1,39 +1,16 @@
-function
-clickbutton(obj) { 
-    let output = document.getElementById('result');
-    if (obj === '1') {
-        output.value += 1;
-    } else if (obj === '2') {
-        output.value += 2;
-    } else if (obj === '3') {
-        output.value += 3;
-    } else if (obj === '4') {
-        output.value += 4;
-    } else if (obj === '5') {
-        output.value += 5;
-    } else if (obj === '6') {
-        output.value += 6;
-    } else if (obj === '7') {
-        output.value += 7;
-    } else if (obj === '8') {
-        output.value += 8;
-    } else if (obj === '9') {
-        output.value += 9;
-    } else if (obj === '0') {
-        output.value += 0;
-    } else if (obj === '+') {
-        output.value += '+';
-    } else if (obj === '-') {
-        output.value += '-';
-    } else if (obj === '*') {
-        output.value += '*';
-    } else if (obj === '/') {
-        output.value += '/';
-    } else if (obj === 'C') {
-        output.value = '';
-    } else if (obj === '<-') {
-        output.value = output.value.substr(0, output.value.length - 1);
-    } else {
-        output.value = eval((output.value))
-    }
+function clickbutton(obj) {
+    let result = document.getElementById('result');
+    result.value += obj;
+}
+function clearscreen(obj) {
+    let result = document.getElementById('result');
+    if (obj === 'C') result.value = '';
+}
+function back_space(obj) {
+    let result = document.getElementById('result');
+    if (obj === '<-') result.value = result.value.substr(0, result.value.length - 1);
+}
+function toTal(obj) {
+    let result = document.getElementById('result');
+    if (obj === '=') result.value = eval((result.value));
 }
