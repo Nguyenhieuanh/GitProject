606071eb249be542892a83ab68480ec4ef9a2bf6
nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-03-06 15:57:35 +0700

    xoa untitled4

--- Thực hành sử dụng toán tử/thuattoan.js
+++
@@ -1,7 +0,0 @@
-function Mul_ti() {
-    let InputWidth = document.getElementById('chieu_rong').value,
-        InputHeight = document.getElementById('chieu_dai').value,
-        wid_th = parseFloat(InputWidth),
-        hei_ght = parseFloat(InputHeight);
-    document.getElementById('dien_tich').value = wid_th * hei_ght;
-}
