6abeb21ed85515eb644f661aa83faa5439c2426e
Nguyenhieuanh <61526323+Nguyenhieuanh@users.noreply.github.com>
2020-03-06 15:54:54 +0700

    Delete 123.js

--- 123.js
+++
@@ -1,8 +0,0 @@
-let a = prompt("nhập giá trị a"),
-    b = prompt("nhập giá trị b"),
-    c = prompt("nhập giá trị c")
-if (a > b && b > c) {
-    alert(a + 'max')
-} else if (b > a && b > c) {
-    alert(b + 'max')
-} else alert(c + 'max')
