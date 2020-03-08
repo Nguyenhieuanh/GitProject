b7759b9a918509785b4157af8a0f68ee01d7310d
nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-03-08 14:17:54 +0700

    doi ten file

--- Calculator/function/Func.js
+++ Calculator/function/Func.js
@@ -1,5 +1,6 @@
 let iscalculator = false;
 let result = document.getElementById('result');
+
 function clickbutton(obj) {
     if (iscalculator) {
         result.value = "";
@@ -7,19 +8,19 @@ function clickbutton(obj) {
     }
     result.value += obj;
 }
+
 function clearscreen(obj) {
     if (obj === 'C') result.value = '';
     if (obj === '<-') result.value = result.value.substr(0, result.value.length - 1);
+    if (obj === 'root') result.value += "**(1/";
 }
+
 function output(obj) {
     iscalculator = true;
     if (obj === '=') result.value = eval((result.value));
     if (obj === 'squrt') result.value = Math.sqrt(result.value);
-    if (obj === '%') result.value = eval((result.value))/100;
-    if (obj === 'sin') result.value = Math.sin(result.value*Math.PI/180);
-    if (obj === 'cos') result.value = Math.cos(result.value*Math.PI/180);
-    if (obj === 'tan') result.value = Math.tan(result.value*Math.PI/180);
+    if (obj === '%') result.value = eval((result.value)) / 100;
+    if (obj === 'sin') result.value = Math.sin(result.value * Math.PI / 180);
+    if (obj === 'cos') result.value = Math.cos(result.value * Math.PI / 180);
+    if (obj === 'tan') result.value = Math.tan(result.value * Math.PI / 180);
 }
-let number = +prompt("Nhap so: ");
-let num = +prompt("Khai can n: ")
-console.log(Math.pow(number,1/num));
